d4548139f40eee8a6ade6626a82e3c54853aef7b
Simone Walter <waltersz@protonmail.com>
2022-06-14 14:08:01 +0200

    Implement translation of intergalactic numerals

--- src/error.rs
+++ src/error.rs
@@ -5,3 +5,20 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 #[error("String is not a valid roman numeral")]
 pub struct InvalidRomanNumeral;
+
+/// `TranslationError` represents an error that results from not recognizing a word or phrase.
+#[derive(Debug, Error)]
+pub enum TranslationError {
+    #[allow(missing_docs)]
+    #[error("{0}")]
+    InvalidRomanNumeral(InvalidRomanNumeral),
+    #[allow(missing_docs)]
+    #[error("Unrecognized word: `{0}`")]
+    UnrecognizedWord(String),
+}
+
+impl From<InvalidRomanNumeral> for TranslationError {
+    fn from(err: InvalidRomanNumeral) -> Self {
+        Self::InvalidRomanNumeral(err)
+    }
+}
--- src/language.rs
+++ src/language.rs
@@ -1,20 +1,84 @@
+//! Language mapping and translator from the intergalactic numeric system to Roman numerals.
+use std::borrow::Cow;
+
 use hashbrown::HashMap;
 use rust_decimal::Decimal;
 
+use crate::error::TranslationError;
 use crate::Roman;
 
-pub struct Language {}
+/// `Language` is a mapping of intergalactic numerals to terran Roman numerals.
+#[derive(Default, Debug, Clone)]
+pub struct Language<'a> {
+    map: HashMap<Cow<'a, str>, char>,
+}
 
-impl Language {
-    pub fn with(map: HashMap<&str, char>) -> Self {
-        todo!()
+impl<'a> Language<'a> {
+    /// Construct an empty `Language`.
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    pub fn translate(&self, text: &str) -> Roman {
-        todo!()
+    /// Construct a `Language` with an existing map.
+    pub fn with(map: HashMap<Cow<'a, str>, char>) -> Self {
+        Self { map }
+    }
+
+    /// Translate an intergalactic numeral to `Roman`.
+    pub fn translate(&self, text: &str) -> Result<Roman, TranslationError> {
+        let text = text
+            // split at whitespace
+            .split(char::is_whitespace)
+            // for every word
+            .map(|word| {
+                // find the translation
+                self.map
+                    .get(&Cow::from(word))
+                    // error if not found
+                    .ok_or_else(|| TranslationError::UnrecognizedWord(word.to_string()))
+            })
+            // collect into string or first error
+            .collect::<Result<String, TranslationError>>();
+        // construct a roman numeral
+        text.and_then(|text| Ok(Roman::try_from(text)?))
     }
 
     pub fn query(&self, price_set: &HashMap<&str, Decimal>, text: &str) -> Decimal {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn translate() {
+        let lang = Language::with(HashMap::from([
+            (Cow::from("glob"), 'I'),
+            (Cow::from("prok"), 'V'),
+            (Cow::from("pish"), 'X'),
+            (Cow::from("tegj"), 'L'),
+        ]));
+
+        // positive tests
+        assert_eq!(
+            lang.translate("glob").unwrap(),
+            Roman::try_from("I").unwrap()
+        );
+        assert_eq!(
+            lang.translate("glob glob glob").unwrap(),
+            Roman::try_from("III").unwrap()
+        );
+        assert_eq!(
+            lang.translate("pish tegj glob glob").unwrap(),
+            Roman::try_from("XLII").unwrap()
+        );
+
+        // negative tests
+        assert!(lang.translate("foo").is_err());
+        assert!(lang.translate("glob foo").is_err());
+        assert!(lang.translate("foo glob").is_err());
+        assert!(lang.translate("glob glob glob glob").is_err());
     }
 }
